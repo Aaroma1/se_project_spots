f6045059205ab2a05988c549cd66389f3e5c90bc
Aaron Romano <aaronjromano1@gmail.com>
2025-03-07 12:40:56 -0800

    uncommented all code

--- scripts/index.js
+++ scripts/index.js
@@ -36,7 +36,7 @@ const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
 const editFormElement = editModal.querySelector(".modal__form");
-// const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
@@ -44,14 +44,13 @@ const editModalDescriptionInput = editModal.querySelector(
 
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector("#add-card-form");
-// const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
+
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaption = previewModal.querySelector(".modal__caption");
-// const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
